15c5f8d8c7641828699df22ee28f50d45a561231
ujjwal9019 <ujjwal9019@gmail.com>
2024-07-29 02:31:12 +0530

    Cors Error Handled

--- src/main/java/com/MovieFlix/demo/auth/config/SecurityConfiguration.java
+++ src/main/java/com/MovieFlix/demo/auth/config/SecurityConfiguration.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -33,8 +35,22 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(authFilterService, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(authFilterService, UsernamePasswordAuthenticationFilter.class).cors();
 
         return http.build();
     }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Adjust to your frontend URL
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
 }
--- src/main/java/com/MovieFlix/demo/controller/AuthController.java
+++ src/main/java/com/MovieFlix/demo/controller/AuthController.java
@@ -11,13 +11,11 @@ import com.MovieFlix.demo.auth.utils.RegisterRequest;
 import com.MovieFlix.demo.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth/")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
 private final AuthService authService;
